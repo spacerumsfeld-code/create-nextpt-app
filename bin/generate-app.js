387262d4c912de8cb7d217c064623dbb8652fb44
Nick Papadakis <nickfin2014@gmail.com>
2021-06-27 18:26:34 -0700

    added imports to generate-app script

---
+++ bin/generate-app.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
