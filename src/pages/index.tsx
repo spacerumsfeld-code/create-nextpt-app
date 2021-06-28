472e9447ddb3e5c5a8eb18c885fa34c3a5174996
Nick Papadakis <nickfin2014@gmail.com>
2021-06-27 17:55:15 -0700

    added typing to basic API route

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+   <Head>
+     <title>Compelling Title</title>
+     <meta name="Home Page" content="Amazing Content" />
+   </Head>
+   <div>I hope create-npt-app saved you some time!</div>
+    </>
+  )
+}
