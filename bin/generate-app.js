98b2692ea0d67beee94deb8164122ee8e8625348
Nick Papadakis <nickfin2014@gmail.com>
2021-06-27 18:29:53 -0700

    added name check and command to script

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,6 +1,29 @@
+#! /usr/bin/env node
+
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { execSync } = require('child_process');
+//necessary imports, eslint ignore for node module importation /w require statement
 const path = require('path');
+const util = require('util');
+const packageJson = require('../package.json');
 const fs = require('fs');
+const exec = util.promisify(require('child_process').exec);
+
+async function runCmd(command) {
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log(stdout);
+    console.log(stderr);
+  } catch {
+    (error) => {
+      console.log('\x1b[31m', error, '\x1b[0m');
+    };
+  }
+}
+
+//ensure app name provided along with create-npt-app command
+if (process.argv.length < 3) {
+  console.log('Please provide a name for your application');
+  process.exit(1);
+}
