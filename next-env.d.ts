b921036594c37249db7769dfffac301b3c97f346
Nick Papadakis <nickfin2014@gmail.com>
2021-06-27 21:11:57 -0700

    added typing for prisma in global context

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+declare global {
+  namespace NodeJS {
+      interface Global {
+          prisma: any;
+      }
+  }
+}
+
+export { }
